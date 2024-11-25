5b866dbfb547d62eda15e576d6079741f960d223
blertaam <mehmetiblerta3@gmail.com>
2024-11-25 17:35:33 +0100

    lesson

---
+++ react-app/TabNavigator.js
@@ -0,0 +1,29 @@
+
+
+const Tab = createBottomTabNavigator();
+
+const createBottomTabNavigator = () =>{
+    return(
+        <Tab.Navigator
+        tabBarOptions ={{
+            activeTintColor: 'white',
+            incativeTintColor: 'lightgray',
+            style: {
+                backgroundColor: '#FF6347',
+            },
+            labelStyle: {
+                fontSize: 12,
+            }
+        }}
+        >
+            <Tab.Screen
+            name = 'Home'
+            component = {{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({color}) => (
+                    <MaterialCommunityIcons name = "home" size={26} color={color}></MaterialCommunityIcons>
+                )
+            }}
+        </Tab.Navigator>
+    )
+}
